5b43d6e8e4cee47d1393a55359e154cdd2586ed7
GreenArrow9 <catbat.pc@gmail.com>
2017-01-03 13:10:17 +0000

    Updates
    - Update README
    - Add colors
    - Add our DND logo

--- reDefined_build.sh
+++ reDefined_build.sh
@@ -1,3 +1,32 @@
+#!/bin/bash
+
+# Specify colors for shell
+red='tput setaf 1'              # red
+green='tput setaf 2'            # green
+yellow='tput setaf 3'           # yellow
+blue='tput setaf 4'             # blue
+violet='tput setaf 5'           # violet
+cyan='tput setaf 6'             # cyan
+white='tput setaf 7'            # white
+txtbld=$(tput bold)             # Bold
+bldred=${txtbld}$(tput setaf 1) # Bold red
+bldgrn=${txtbld}$(tput setaf 2) # Bold green
+bldblu=${txtbld}$(tput setaf 4) # Bold blue
+bldcya=${txtbld}$(tput setaf 6) # Bold cyan
+normal='tput sgr0'
+
+./vendor/cm/colors
 echo -e "";
+tput setaf 1
+echo "                                   ";
+echo " _|_|_|     _|      _|   _|_|_|    ";
+echo " _|    _|   _|_|    _|   _|    _|  ";
+echo " _|    _|   _|  _|  _|   _|    _|  ";
+echo " _|    _|   _|    _|_|   _|    _|  ";
+echo " _|_|_|     _|      _|   _|_|_|    ";
+echo "                                   ";
+echo "                                   ";
+echo -e "";
+tput setaf 2
 echo -e " √ Build is Successfully Done! ";
 echo -e "";
