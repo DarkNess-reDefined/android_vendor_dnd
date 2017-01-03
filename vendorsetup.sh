6b5701a4c4270bf8a415890f59af51a6b969676c
GreenArrow9 <catbat.pc@gmail.com>
2017-01-03 13:53:44 +0000

    Vendorsetup update
    - We are based on cm & we build on cm(LineageOS) makes file
    - Stop getting device list from LineageOS github
    - Introduce the device list we supported
    - Switch to eng builds for now

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for combo in $(curl -s https://raw.githubusercontent.com/LineageOS/hudson/master/cm-build-targets | sed -e 's/#.*$//' | grep cm-14.1 | awk '{printf "cm_%s-%s\n", $1, $2}')
+for device in $(cat vendor/cm/dnd.devices)
 do
-    add_lunch_combo $combo
+add_lunch_combo cm_$device-eng
 done
