2ef9935c614264b92502e999ed607e97187c2334
AKabhishek <abhishekkaushik.star@gmail.com>
2017-01-21 13:04:59 +0000

    vendor: updates
    - userinit support in all buildtype
    - chromium will come to darkness package now

--- config/common.mk
+++ config/common.mk
@@ -67,13 +67,8 @@ PRODUCT_COPY_FILES += \
 # init.d support
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/cm/prebuilt/common/bin/sysinit:system/bin/sysinit
-
-ifneq ($(TARGET_BUILD_VARIANT),user)
-# userinit support
-PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/bin/sysinit:system/bin/sysinit \
     vendor/cm/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
-endif
 
 # CM-specific init file
 PRODUCT_COPY_FILES += \
