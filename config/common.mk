d68b79d4609eec183593c20be45e978607030677
DarkKnight6499 <yazad.madan@gmail.com>
2017-01-21 09:42:58 +0000

    Remove unnecessary su system dependency

--- config/common.mk
+++ config/common.mk
@@ -242,7 +242,6 @@ endif
 # SuperSU
 PRODUCT_COPY_FILES += \
     vendor/cm/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
-    vendor/cm/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
 
 DEVICE_PACKAGE_OVERLAYS += vendor/cm/overlay/common
 
