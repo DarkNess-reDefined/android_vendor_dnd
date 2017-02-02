a8a4ce439599213cb9d40427d182cf624d1a5911
AKabhishek <abhishekkaushik.star@gmail.com>
2017-02-02 05:04:27 +0000

    Remove cmte stuffs
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -93,7 +93,7 @@ PRODUCT_COPY_FILES += \
 include vendor/dnd/config/dnd_audio.mk
 
 # Theme engine
-include vendor/dnd/config/themes_common.mk
+#include vendor/dnd/config/themes_common.mk
 
 ifneq ($(TARGET_DISABLE_CMSDK), true)
 # CMSDK
