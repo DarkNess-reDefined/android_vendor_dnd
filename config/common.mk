c1e2f1085f4a9ff7682764d4b5ef2b2cfef4a92f
Abhishek Kaushik <abhishekkaushik.star@gmail.com>
2017-02-14 17:53:07 +0530

    Clean up duplication

--- config/common.mk
+++ config/common.mk
@@ -17,9 +17,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
-    ro.com.android.dataroaming=false \
-    ro.opa.eligible_device=true
-
+    ro.com.android.dataroaming=false
+    
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.selinux=1
 
