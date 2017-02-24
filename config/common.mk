bf4e05392f9f15d7132eb9a35f215f1e69d02981
AKabhishek <abhishekkaushik.star@gmail.com>
2017-02-24 07:37:52 +0000

    Welcome v1.1
    - Changelogs :- Darkness-reDefined/reDefinition_documentation@2a3092c
    
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -262,7 +262,7 @@ PRODUCT_PACKAGES += \
     SlimOTA
 endif
 
-PRODUCT_VERSION = 1.0
+PRODUCT_VERSION = 1.1
 ifneq ($(DND_BUILDTYPE),)
 CM_VERSION := DarkNess-reDefined-N-v$(PRODUCT_VERSION)-$(shell date -u +%Y%m%d)-$(CM_BUILD)-$(DND_BUILDTYPE)
 else
