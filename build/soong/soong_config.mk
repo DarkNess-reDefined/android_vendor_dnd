0a13a97a32402b881907e1fe9b530bf15c573abf
Simon Shields <simon@lineageos.org>
2017-09-04 21:11:16 +1000

    build: soong: put dnd variables in the "Darkness" struct
    Change-Id: I9ed55cea0c55f0078f8f97ad4037b16ed11c0156
    Signed-off-by: DarkKnight6499 <yazad.madan@gmail.com>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,5 +1,8 @@
+# Insert new variables inside the DND structure
 lineage_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '"DarkNess-reDefined": {'; \
+	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
