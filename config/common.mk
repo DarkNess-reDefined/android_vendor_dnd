AospExtended/platform_vendor_aosp

common: Fix paths for camera effects
Change-Id: I6e26023cc2fc6a756a4253ba14cc9720f31d7d33

config/common.mk
@@ -105,8 +105,8 @@ PRODUCT_COPY_FILES += \
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
-    vendor/aosp/prebuilt/common/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
-    vendor/aosp/prebuilt/common/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
+    vendor/aosp/prebuilt/common/media/LMspeed_508.emd:system/media/LMspeed_508.emd \
+    vendor/aosp/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
 # Copy over added mimetype supported in libcore.net.MimeUtils
 PRODUCT_COPY_FILES += \

DarkNess-reDefined/android_vendor_dnd

Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

config/common.mk
@@ -113,8 +113,8 @@ PRODUCT_COPY_FILES += \
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
-    vendor/dnd/prebuilt/common/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
-    vendor/dnd/prebuilt/common/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
+    vendor/dnd/prebuilt/common/media/LMspeed_508.emd:system/media/LMspeed_508.emd \
+    vendor/dnd/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
 
 # Exchange support
 PRODUCT_PACKAGES += \